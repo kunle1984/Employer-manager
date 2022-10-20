497d827b77f166cd779d6b8220590cd691c39cb1
kunle <kunlefes089@gmail.com>
2022-10-21 00:21:38 +0100

    change settings

--- EmployeeApp/settings.py
+++ EmployeeApp/settings.py
@@ -129,10 +129,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_ROOT=os.path.join(BASE_DIR, "Photos")
 MEDIA_URL='/Photos/'
